63323c881029adf557ace682c4fd956e253addaf
mercuzzi <amercnik@gmail.com>
2020-04-12 13:48:01 -0400

    removed a few listing in about us updated logo changed version number

--- app/src/main/java/com/cryptape/cita_wallet/activity/AboutUsActivity.java
+++ app/src/main/java/com/cryptape/cita_wallet/activity/AboutUsActivity.java
@@ -15,8 +15,7 @@ public class AboutUsActivity extends NBaseActivity {
 
     private TextView versionText;
     private TitleBar title;
-    private SettingButtonView sourceCodeSBV, serverPrivateSBV, NervosSBV, InfuraSBV, openSeaSBV, peckShieldSBV, citaSBV;
-
+    private SettingButtonView sourceCodeSBV, serverPrivateSBV, InfuraSBV, openSeaSBV;
     @Override
     protected int getContentLayout() {
         return R.layout.activity_about_us;
@@ -27,12 +26,11 @@ public class AboutUsActivity extends NBaseActivity {
         versionText = findViewById(R.id.app_version);
         sourceCodeSBV = findViewById(R.id.sbv_source_code);
         serverPrivateSBV = findViewById(R.id.sbv_server_private);
-        NervosSBV = findViewById(R.id.sbv_nervos_network);
         InfuraSBV = findViewById(R.id.sbv_infura);
         openSeaSBV = findViewById(R.id.sbv_open_sea);
         title = findViewById(R.id.title);
-        peckShieldSBV = findViewById(R.id.sbv_peck_shield);
-        citaSBV = findViewById(R.id.sbv_cita);
+
+
     }
 
     @Override
@@ -59,16 +57,12 @@ public class AboutUsActivity extends NBaseActivity {
         serverPrivateSBV.setOnClickListener(() -> {
             SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.PRODUCT_AGREEMENT_URL);
         });
-        NervosSBV.setOnClickListener(() -> {
-            SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.NERVOS_WEB_URL);
-        });
         InfuraSBV.setOnClickListener(() -> {
             SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.INFURA_URL);
         });
         openSeaSBV.setOnClickListener(() -> {
             SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.OPEN_SEA_URL);
         });
-        peckShieldSBV.setOnClickListener(() -> SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.PECKSHEILD_URL));
-        citaSBV.setOnClickListener(() -> SimpleWebActivity.gotoSimpleWeb(AboutUsActivity.this, HttpUrls.CITA_GITHUB_URL));
+
     }
 }
